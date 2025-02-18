d77591ee182a1044380dc5bde286009246e84b99
Camila Diaz <100711870+camdiaz@users.noreply.github.com>
2025-02-17 20:06:03 -0500

    Update config.h

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // Configuraciones de Wi-Fi
-#define WIFI_SSID "ASTROMELIA"        // Reemplaza "Tu_SSID" por el nombre de tu red Wi-Fi
-#define WIFI_PASSWORD "27310405"  // Reemplaza "Tu_PASSWORD" por la contraseña de tu red Wi-Fi
+#define WIFI_SSID "iot"        // Reemplaza "Tu_SSID" por el nombre de tu red Wi-Fi
+#define WIFI_PASSWORD "11111"  // Reemplaza "Tu_PASSWORD" por la contraseña de tu red Wi-Fi
 
 #endif
